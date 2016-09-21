6785675cfb9830a88af13eddb83a950e053cff75
LouisVN <lvn.vanniekerk@gmail.com>
2016-09-21 12:50:33 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20161021101447) do
 
-  create_table "education_levels", force: :cascade do |t|
-    t.string "name", limit: 255
-  end
-
-  create_table "education_levels_standards", id: false, force: :cascade do |t|
-    t.integer "standard_id",        limit: 4, null: false
-    t.integer "education_level_id", limit: 4, null: false
-  end
-
-  add_index "education_levels_standards", ["education_level_id"], name: "index_education_levels_standards_on_education_level_id", using: :btree
-  add_index "education_levels_standards", ["standard_id"], name: "index_education_levels_standards_on_standard_id", using: :btree
-
   create_table "jurisdictions", force: :cascade do |t|
     t.string "csp_id",   limit: 255
     t.string "document", limit: 255
@@ -33,17 +21,6 @@ ActiveRecord::Schema.define(version: 20161021101447) do
   end
 
   add_index "jurisdictions", ["csp_id"], name: "index_jurisdictions_on_csp_id", using: :btree
-
-  create_table "parents", force: :cascade do |t|
-  end
-
-  create_table "parents_standards", id: false, force: :cascade do |t|
-    t.integer "standard_id", limit: 4, null: false
-    t.integer "parent_id",   limit: 4, null: false
-  end
-
-  add_index "parents_standards", ["parent_id"], name: "index_parents_standards_on_parent_id", using: :btree
-  add_index "parents_standards", ["standard_id"], name: "index_parents_standards_on_standard_id", using: :btree
 
   create_table "standards", force: :cascade do |t|
     t.integer "jurisdiction_id", limit: 4,                   null: false
@@ -58,15 +35,29 @@ ActiveRecord::Schema.define(version: 20161021101447) do
   add_index "standards", ["csp_id"], name: "index_standards_on_csp_id", using: :btree
   add_index "standards", ["jurisdiction_id"], name: "index_standards_on_jurisdiction_id", using: :btree
 
+  create_table "standards_education_levels", id: false, force: :cascade do |t|
+    t.integer "standard_id",     limit: 4,   default: 0,  null: false
+    t.string  "education_level", limit: 255, default: "", null: false
+  end
+
+  add_index "standards_education_levels", ["standard_id"], name: "index_standards_education_levels_on_standard_id", using: :btree
+
   create_table "standards_schema_migrations", id: false, force: :cascade do |t|
     t.string "version", limit: 255, null: false
   end
 
   add_index "standards_schema_migrations", ["version"], name: "unique_schema_migrations", unique: true, using: :btree
 
-  add_foreign_key "education_levels_standards", "education_levels"
-  add_foreign_key "education_levels_standards", "standards"
-  add_foreign_key "parents_standards", "parents"
-  add_foreign_key "parents_standards", "standards"
+  create_table "standards_standards", id: false, force: :cascade do |t|
+    t.integer "parent_id", limit: 4, default: 0, null: false
+    t.integer "child_id",  limit: 4, default: 0, null: false
+  end
+
+  add_index "standards_standards", ["child_id"], name: "index_standards_standards_on_child_id", using: :btree
+  add_index "standards_standards", ["parent_id"], name: "index_standards_standards_on_parent_id", using: :btree
+
   add_foreign_key "standards", "jurisdictions"
+  add_foreign_key "standards_education_levels", "standards"
+  add_foreign_key "standards_standards", "standards", column: "child_id"
+  add_foreign_key "standards_standards", "standards", column: "parent_id"
 end
